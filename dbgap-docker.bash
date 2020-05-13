2ffeb16161406f368d777b8dbe151c4228a9a373
dale-conklin <conklin.dale@gmail.com>
2020-05-13 16:18:02 +0000

    Bash script to run docker-compose. Populates .env file for use by docker-compse to set variables on the file and set up docker containers to run dbGaP pre-processing validation.

--- dbgap-docker.bash
+++ dbgap-docker.bash
@@ -4,8 +4,9 @@
 # HEADER
 #================================================================
 #% SYNOPSIS
-#+    ${SCRIPT_NAME} [-h] [-i [input directory]] [-o [output directory]] [-t [local|nfs]]
-#%
+#+    ${SCRIPT_NAME} [-hv] [-i [input directory]] [-o [output directory]] [up|down]
+#==  [-t [local|nfs]]  to be used later
+#+
 #% DESCRIPTION
 #%    This script runs the NIH NCBI tool to validate data
 #%    submitted to the dbGaP database. It loads a docker-compose
@@ -13,16 +14,21 @@
 #%
 #%
 #% OPTIONS
-#%    -o [file], --output=[file]    Set log file (default=/dev/null)
-#%                                  use DEFAULT keyword to autoname file
-#%                                  The default value is /dev/null.
-#%    -t, --timelog                 Add timestamp to log ("+%y/%m/%d@%H:%M:%S")
-#%    -x, --ignorelock              Ignore if lock file exists
-#%    -h, --help                    Print this help
-#%    -v, --version                 Print script information
+#%    -i [input directory]          Input directory containing the 
+#%                                  data files to be validated.
+#%    -o                            Output directory
+#%    -h                            Print this help
+#%    -v                            Print script information
 #%
 #% EXAMPLES
-#%    ${SCRIPT_NAME} -o DEFAULT arg1 arg2
+#%    ${SCRIPT_NAME} -i ./input_files -o ./output_files up
+#%    ${SCRIPT_NAME} down
+#%
+#%
+#% REQUIREMENTS
+#%    docker
+#%    docker-compose
+#%
 #%
 #================================================================
 #- IMPLEMENTATION
@@ -30,6 +36,7 @@
 #-    author          Dale Conklin
 #-    copyright       Copyright (c) 2020
 #-    license         GNU General Public License
+#-
 #-
 #================================================================
 #  HISTORY
@@ -50,61 +57,102 @@ usage() { printf "Usage: "; head -${SCRIPT_HEADSIZE:-99} ${0} | grep -e "^#+" | 
 usagefull() { head -${SCRIPT_HEADSIZE:-99} ${0} | grep -e "^#[%+-]" | sed -e "s/^#[%+-]//g" -e "s/\${SCRIPT_NAME}/${SCRIPT_NAME}/g" ; }
 scriptinfo() { head -${SCRIPT_HEADSIZE:-99} ${0} | grep -e "^#-" | sed -e "s/^#-//g" -e "s/\${SCRIPT_NAME}/${SCRIPT_NAME}/g"; }
 
-while getopts "ht:i:o:" option; do
-   case $option in
-      h) # display Help
-         usage
-         exit
-         ;;
-      t) # type of storage
-         FS_TYPE=$OPTARG
-         FS_TYPE="$(echo -e "${FS_TYPE,,}" | tr -d '[[:space:]]')"
-            if ! [[ "${FS_TYPE}" =~ ^(local|nfs)$ ]]; then
-               echo "Valid options for filesystem type are local or nfs."
+
+##################
+# Parse and verify command line options
+##################
+OPTIONS=hvt:i:o:
+LONGOPTS=help,input:,output:,type:
+
+# pass arguments only via   -- "$@"   to separate them correctly
+! PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
+if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+    # e.g. return value is 1
+    #  then getopt has complained about wrong arguments to stdout
+    echo "Wrong number/type of arguments"
+    usage
+    exit 2
+fi
+# read getopt output to handle the quoting:
+eval set -- "$PARSED"
+
+# interate through to set values for the script
+while true; do
+    case "$1" in
+        -h|--help)
+            usagefull
+            exit
+            ;;
+        -v)
+            scriptinfo
+            exit
+            ;;
+        -i|--input)
+            echo $2
+            INPUT_DIR="$2" 
+            INPUT_DIR="$(echo -e "${INPUT_DIR}" | tr -d '[[:space:]]')"
+            if [ -z "$INPUT_DIR" ]; then
+               echo $INPUT_DIR " input directory is a problem"
                usage
                exit 2
             fi
-            if [ -z "$FS_TYPE" ]; then
-               echo $FS_TYPE " Filesystem type problem"
-               exit 2
-            fi
-            if [ z"${OPTARG:0:1}" == "z-" ]; then
-               echo $FS_TYPE " Looks like filesystem was set with an option string"
-               exit 2
-            fi 
-            ;;
-      i) # input directory
-         INPUT_DIR=$OPTARG 
-         INPUT_DIR="$(echo -e "${INPUT_DIR}" | tr -d '[[:space:]]')"
-            if [ -z "$INPUT_DIR" ]; then
-               echo $INPUT_DIR " input directory is a problem"
-               exit 2
-            fi
-            if [ z"${OPTARG:0:1}" == "z-" ]; then
+            if [ z"${INPUT_DIR:0:1}" == "z-" ]; then
                echo $INPUT_DIR " Looks like input directory was set with an option string"
+               usage
                exit 2
-            fi 
+            fi
+            shift 2 
             ;;
-      o) # output directory
-         OUTPUT_DIR=$OPTARG 
-         OUTPUT_DIR="$(echo -e "${OUTPUT_DIR}" | tr -d '[[:space:]]')"
+        -o|--output)
+            OUTPUT_DIR="$2" 
+            OUTPUT_DIR="$(echo -e "${OUTPUT_DIR}" | tr -d '[[:space:]]')"
             if [ -z "$OUTPUT_DIR" ]; then
                echo $OUTPUT_DIR "Output directory has problem"
+               usage
                exit 2
             fi
-            if [ z"${OPTARG:0:1}" == "z-" ]; then
+            if [ z"${OUTPUT_DIR:0:1}" == "z-" ]; then
                echo $OUTPUT_DIR "Output directory was set with an option string"
+               usage
                exit 2
             fi 
+            shift 2
             ;;
-      ?) echo "we have a problem"; usage
-         exit 2
-         ;;
-      :) 
-         echo "Invalid option: $OPTARG requires an argument"; usage 1>&2
-         exit 2 ;;
-   esac
+        --)
+            shift
+            break
+            ;;
+        *)
+            echo "An incorrect option has been supplied"
+            usage
+            exit 3
+            ;;
+    esac
 done
+
+##################
+# Verify up/down arguments were supplied properly
+##################
+if [[ $# -ne 1 ]]; then
+    echo $'\n'"$0: options supplied or argument is incorrect."$'\n'
+    usage
+    exit 4
+fi
+DSTATE=$1
+if ! [[ "${DSTATE}" =~ ^(up|down)$ ]]; then
+    echo $'\n'"$0: up or down are the only allowed arguments."$'\n'
+    usage
+    exit 4
+fi
+
+#########################
+# Run the docker-compose commands to bring the environment down
+# Skips the rest of the validation and exit the script
+#########################
+if [ $DSTATE == "down" ]; then
+   docker-compose -f docker-compose-CeleryExecutor.yml down
+   exit
+fi
 
 ########################
 # Validate that all options have been set
@@ -116,17 +164,38 @@ fi
 if [ -z ${OUTPUT_DIR} ]; then
    ERROR_TEXT=$ERROR_TEXT$'Output directory has not been set.\n'
 fi
-if [ -z ${FS_TYPE} ]; then
-   ERROR_TEXT=$ERROR_TEXT$'Filesystem type has not been set. Valid options are local or nfs.\n'
-fi
+# if [ -z ${FS_TYPE} ]; then
+#    ERROR_TEXT=$ERROR_TEXT$'Filesystem type has not been set. Valid options are local or nfs.\n'
+# fi
 if [ ! -z "${ERROR_TEXT}" ]; then
    echo "$ERROR_TEXT"
    usage
    exit 2
 fi
 
-#########################
-# Replace all variables in the template file
-#########################
+########################
+# Validate that the input and output directories exist
+########################
+if [ ! -d "$INPUT_DIR" ]; then
+   echo "Input directory does not exist"
+   exit 2
+fi
+if [ ! -d "$OUTPUT_DIR" ]; then
+   echo "Output directory does not exist"
+   exit 2
+fi
 
-echo "Filesystem |"$FS_TYPE"| input directory: |"$INPUT_DIR"| " 
+########################
+# Create a .env file to be used by docker-compose
+########################
+echo "OUTPUT_VOL=${OUTPUT_DIR}" > .env
+echo "INPUT_VOL=${INPUT_DIR}" >> .env
+
+
+#########################
+# Run the docker-compose commands to bring the environment up
+#########################
+if [ $DSTATE == "up" ]; then
+   docker-compose -f docker-compose-CeleryExecutor.yml up -d
+fi
+
